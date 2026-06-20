6f8b41e598a114bb2c45e8cb1e051b5e1b21baed
TANISHK SINGH <tanishksingh4444@gmail.com>
2026-06-20 15:07:10 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,14 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is dedicated to providing high-quality houseplants
+        and gardening products to plant enthusiasts. Our mission is to make
+        homes greener and healthier through beautiful indoor plants.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
