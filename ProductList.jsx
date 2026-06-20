8321fdac18c8b6ccc131783efd078423e6cf0c5e
TANISHK SINGH <tanishksingh4444@gmail.com>
2026-06-20 15:20:02 +0530

    Create ProductList.jsx

---
+++ ProductList.jsx
@@ -0,0 +1,28 @@
+const plants = {
+  Indoor: [
+    { id: 1, name: "Snake Plant", price: 15, image: "image1.jpg" },
+    { id: 2, name: "Peace Lily", price: 20, image: "image2.jpg" },
+    { id: 3, name: "Spider Plant", price: 18, image: "image3.jpg" },
+    { id: 4, name: "ZZ Plant", price: 22, image: "image4.jpg" },
+    { id: 5, name: "Pothos", price: 12, image: "image5.jpg" },
+    { id: 6, name: "Aloe Vera", price: 10, image: "image6.jpg" }
+  ],
+
+  Succulents: [
+    { id: 7, name: "Jade Plant", price: 14, image: "image7.jpg" },
+    { id: 8, name: "Echeveria", price: 16, image: "image8.jpg" },
+    { id: 9, name: "Haworthia", price: 12, image: "image9.jpg" },
+    { id: 10, name: "Burro Tail", price: 18, image: "image10.jpg" },
+    { id: 11, name: "Panda Plant", price: 15, image: "image11.jpg" },
+    { id: 12, name: "Lithops", price: 20, image: "image12.jpg" }
+  ],
+
+  Flowering: [
+    { id: 13, name: "Rose", price: 25, image: "image13.jpg" },
+    { id: 14, name: "Orchid", price: 30, image: "image14.jpg" },
+    { id: 15, name: "Hibiscus", price: 22, image: "image15.jpg" },
+    { id: 16, name: "Jasmine", price: 18, image: "image16.jpg" },
+    { id: 17, name: "Marigold", price: 10, image: "image17.jpg" },
+    { id: 18, name: "Lavender", price: 20, image: "image18.jpg" }
+  ]
+};
